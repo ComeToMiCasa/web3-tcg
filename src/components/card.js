c26fbe597be1964d28b121c83bf591e548d242f0
ComeToMiCasa <rk.ca.tsiak@kaist.ac.kr>
2023-05-24 00:03:22 +0900

    Complete inventory skeleton

--- src/components/card.js
+++ src/components/card.js
@@ -1,9 +1,19 @@
-import React from "react"
+import React, { useEffect } from "react"
+import "../styles/card.css"
 
-const Card = () => {
+const Card = ({ name, text, image }) => {
+	useEffect(() => console.log("name", name), [])
 	return (
-		<div>
-            card
+		<div className="Card">
+			<div className="ImageContainer">
+				<img src={image} className="Image"></img>
+			</div>
+			<div className="NameContainer">
+				{name}
+			</div>
+			<div className="TextContainer">
+				{text}
+			</div>
 		</div>
 	)
 }
--- src/pages/inventoryPage.js
+++ src/pages/inventoryPage.js
@@ -1,14 +1,16 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { db, storage } from "../firebase"
-import { addDoc, collection } from "firebase/firestore"
+import { addDoc, collection, getDocs, query } from "firebase/firestore"
 import { getDownloadURL, ref } from "firebase/storage"
+import Card from "../components/card"
+import "../styles/card.css"
 
 const InventoryPage = () => {
 
-	const cardList = []
+	const [cards, setCards] = useState([])
 
 	// db에 카드 추가용 매크로
-	const setCards = async () => {
+	const addCardToDB = async () => {
 		const imageRef = ref(storage, "tmp.jpeg")
 		const imageURL = await getDownloadURL(imageRef)
 
@@ -21,10 +23,33 @@ const InventoryPage = () => {
 		})
 	}
 
+	const getCards = async () => {
+		const cardRef = collection(db, "Cards")
+		const q = query(cardRef)
+
+		const querySnapshot = await getDocs(q)
+		const cards = querySnapshot.docs.map((docSnapshot) => {
+			const { name, text, image } = docSnapshot.data()
+			return { name, text, image }
+		})
+		setCards(cards)
+	}
+
+	useEffect(() => {
+		getCards()
+	}, [])
+
+	const cardList = cards.map((card, i) => {
+		console.log("card", card)
+		return (<Card {...card} key={i} />)
+	})
+
 	return (
-		<div>
-            Inventory
-			{/* <button onClick ={setCards}>Add Card</button> */}
+		<div className="Inventory">
+			<div style={{width: 1500}}>
+				inventory
+			</div>
+			{cardList}
 		</div>
 	)
 }
